78b4c6f07c2f5bad988b181c2fb12fc5e65719ac
ritikNegi2022 <negiritik2022@gmail.com>
2023-01-21 14:34:34 +0530

    ''

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,15 +5,15 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(process.env.DB_URL);
-      const db = client.db();
+    const db = client.db();
 
-      const meetupCollection = db.collection('meetups')
-      const result = await meetupCollection.insertOne({data})
-      console.log(result);
+    const meetupCollection = db.collection("meetups");
+    const result = await meetupCollection.insertOne({ data });
+    console.log(result);
 
-      client.close()
+    client.close();
 
-      res.status(201).json({ message: 'Meetup inserted' });
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
